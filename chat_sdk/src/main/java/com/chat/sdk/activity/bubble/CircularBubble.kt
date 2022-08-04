5f481a5c3b2b8a05333939c1d48d8d425d7578b3
AjayM17 <ajaymandrawal17@gmai.com>
2022-08-04 15:59:40 +0530

    Custom Bubble Form Class

--- chat_sdk/src/main/java/com/chat/sdk/activity/bubble/CircularBubble.kt
+++ chat_sdk/src/main/java/com/chat/sdk/activity/bubble/CircularBubble.kt
@@ -1,10 +1,10 @@
 package com.chat.sdk.activity.bubble
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -15,150 +15,125 @@ import com.chat.sdk.R
 import com.chat.sdk.modal.ChatStyle
 import com.chat.sdk.network.BaseUrl
 import com.chat.sdk.util.ScreenUtil
+import com.mikhaellopez.circularimageview.CircularImageView
 
-internal class CircularBubble {
-    @SuppressLint("InflateParams")
+internal class CircularBubble() {
     fun configureCircularBubble(view: View, chatStyle: ChatStyle) {
         val layout = view.findViewById<ConstraintLayout>(R.id.bubble_layout)
-        val layoutParams = layout.layoutParams
-//        layoutParams.width = ViewGroup.LayoutParams.WRAP_CONTENT
-//        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
         val circularIconView =
             LayoutInflater.from(view.context).inflate(R.layout.circular_icon, null)
         val circularIconLayout =
             circularIconView.findViewById<LinearLayout>(R.id.circular_bubble_layout)
-//        circularIconLayout.layoutParams = ViewGroup.LayoutParams(
-//            ViewGroup.LayoutParams.MATCH_PARENT,
-//            ViewGroup.LayoutParams.MATCH_PARENT
-//        )
-
+        circularIconLayout.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        val unwrappedDrawable =
+            AppCompatResources.getDrawable(view.context, R.drawable.circular_bubble_bg)
+        val wrappedDrawable = unwrappedDrawable?.let { DrawableCompat.wrap(it) }
+        if (wrappedDrawable != null) {
+            DrawableCompat.setTint(wrappedDrawable, Color.parseColor("#${chatStyle.chead_color}"))
+        }
+        circularIconLayout.background = wrappedDrawable
         configureCircularBubbleType(circularIconLayout, chatStyle)
         layout.addView(circularIconView)
         setBubbleIconConstraint(layout)
         layout.addView(onlineIcon(view))
-        setOnlineIconConstraint(layout,chatStyle)
+        setOnlineIconConstraint(layout)
     }
 
     private fun configureCircularBubbleType(
         circularIconLayout: LinearLayout,
         chatStyle: ChatStyle
     ) {
+
         val icon = circularIconLayout.findViewById<ImageView>(R.id.icon)
         val text = circularIconLayout.findViewById<TextView>(R.id.text)
-        val customIcon = circularIconLayout.findViewById<ImageView>(R.id.custom_icon)
-        val staticImage = circularIconLayout.findViewById<ImageView>(R.id.static_image)
+        val customIcon = circularIconLayout.findViewById<CircularImageView>(R.id.custom_icon)
         when (chatStyle.embedded_window) {
             CircularBubbleType.CUSTOM_URL.type -> {
                 icon.visibility = View.GONE
                 text.visibility = View.GONE
-                staticImage.visibility = View.GONE
                 customIcon.visibility = View.VISIBLE
+                customIcon.layoutParams.width = ScreenUtil().getCustomImageWidth(customIcon.context)
+                customIcon.layoutParams.height = ScreenUtil().getCustomImageWidth(customIcon.context)
                 Glide
                     .with(circularIconLayout.context)
                     .load("${BaseUrl.ImageUrl}${chatStyle.custom_chat_bubble}")
-                    .circleCrop()
+                    .centerCrop()
                     .into(customIcon)
-            }
-            CircularBubbleType.MALE.type -> {
-                icon.visibility = View.GONE
-                text.visibility = View.GONE
-                customIcon.visibility = View.GONE
-                staticImage.visibility = View.VISIBLE
-                staticImage.setImageResource(R.drawable.male)
-            }
-            CircularBubbleType.FEMALE.type -> {
-                icon.visibility = View.GONE
-                text.visibility = View.GONE
-                customIcon.visibility = View.GONE
-                staticImage.visibility = View.VISIBLE
-                staticImage.setImageResource(R.drawable.female)
             }
 
             CircularBubbleType.TEXT_HELP.type -> {
-                addBackgroundInBubble(circularIconLayout,chatStyle)
                 icon.visibility = View.GONE
                 customIcon.visibility = View.GONE
-                staticImage.visibility = View.GONE
                 text.visibility = View.VISIBLE
                 text.typeface = Typeface.DEFAULT_BOLD
                 text.text = text.context.getString(R.string.help)
             }
             CircularBubbleType.TEXT_CHAT.type -> {
-                addBackgroundInBubble(circularIconLayout,chatStyle)
                 icon.visibility = View.GONE
                 customIcon.visibility = View.GONE
-                staticImage.visibility = View.GONE
                 text.visibility = View.VISIBLE
                 text.typeface = Typeface.DEFAULT_BOLD
                 text.text = text.context.getString(R.string.chat)
             }
             else -> {
-                addBackgroundInBubble(circularIconLayout, chatStyle)
                 text.visibility = View.GONE
                 customIcon.visibility = View.GONE
-                staticImage.visibility = View.GONE
                 icon.visibility = View.VISIBLE
-                configureIcon(icon, chatStyle)
+                configureIcon(icon, chatStyle.embedded_window)
             }
         }
     }
 
 
-    private fun addBackgroundInBubble(
-        circularIconLayout: LinearLayout,
-        chatStyle: ChatStyle
-    ) {
-        val unwrappedDrawable =
-            AppCompatResources.getDrawable(
-                circularIconLayout.context,
-                R.drawable.circular_bubble_bg
-            )
-        val wrappedDrawable = unwrappedDrawable?.let { DrawableCompat.wrap(it) }
-        if (wrappedDrawable != null) {
-            DrawableCompat.setTint(wrappedDrawable, Color.parseColor("#${chatStyle.chead_color}"))
-        }
-        circularIconLayout.background = wrappedDrawable
-    }
+    private fun configureIcon(icon: ImageView, type: String) {
 
-    private fun configureIcon(
-        icon: ImageView,
-        chatStyle: ChatStyle
-    ) {
-        when (chatStyle.embedded_window) {
+        when (type) {
             CircularBubbleType.ICON_7.type -> {
-//                icon.layoutParams.width = ScreenUtil().getCircularIconWidth(icon.context)
-//                icon.layoutParams.height = ScreenUtil().getCircularIconWidth(icon.context)
+                icon.layoutParams.width = ScreenUtil().getCircularIconWidth(icon.context)
+                icon.layoutParams.height = ScreenUtil().getCircularIconWidth(icon.context)
                 icon.setImageResource(R.drawable.seven)
             }
             CircularBubbleType.ICON_10.type -> {
-//                icon.layoutParams.width = ScreenUtil().getCircularIconWidth(icon.context)
-//                icon.layoutParams.height = ScreenUtil().getCircularIconWidth(icon.context)
+                icon.layoutParams.width = ScreenUtil().getCircularIconWidth(icon.context)
+                icon.layoutParams.height = ScreenUtil().getCircularIconWidth(icon.context)
                 icon.setImageResource(R.drawable.ten)
             }
             CircularBubbleType.ICON_12.type -> {
-//                icon.layoutParams.width = ScreenUtil().getCircularIconWidth(icon.context)
-//                icon.layoutParams.height = ScreenUtil().getCircularIconWidth(icon.context)
+                icon.layoutParams.width = ScreenUtil().getCircularIconWidth(icon.context)
+                icon.layoutParams.height = ScreenUtil().getCircularIconWidth(icon.context)
                 icon.setImageResource(R.drawable.twelve)
             }
             CircularBubbleType.ICON_3.type -> {
-//                icon.layoutParams.width = ScreenUtil().getCircularIconWidth(icon.context)
-//                icon.layoutParams.height = ScreenUtil().getCircularIconWidth(icon.context)
+                icon.layoutParams.width = ScreenUtil().getCircularIconWidth(icon.context)
+                icon.layoutParams.height = ScreenUtil().getCircularIconWidth(icon.context)
                 icon.setImageResource(R.drawable.third)
             }
-
+            CircularBubbleType.MALE.type -> {
+                icon.layoutParams.width = ScreenUtil().getImageViewBubbleWidth(icon.context)
+                icon.layoutParams.height = ScreenUtil().getImageViewBubbleWidth(icon.context)
+                icon.setImageResource(R.drawable.male)
+            }
+            CircularBubbleType.FEMALE.type -> {
+                icon.layoutParams.width = ScreenUtil().getImageViewBubbleWidth(icon.context)
+                icon.layoutParams.height = ScreenUtil().getImageViewBubbleWidth(icon.context)
+                icon.setImageResource(R.drawable.female)
+            }
 
         }
     }
 
-    @SuppressLint("InflateParams")
     private fun onlineIcon(view: View): View? {
         val iconLayout =
             LayoutInflater.from(view.context).inflate(R.layout.online_status_layout, null)
-//        iconLayout.layoutParams = LinearLayout.LayoutParams( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val width = ScreenUtil().getScreenWidth(view.context) / 5
+        iconLayout.layoutParams = LinearLayout.LayoutParams(width, width)
         return iconLayout
     }
 
-    private fun setOnlineIconConstraint(circularView: ConstraintLayout,chatStyle: ChatStyle) {
+    private fun setOnlineIconConstraint(circularView: ConstraintLayout) {
         val constraintSet = ConstraintSet()
         constraintSet.clone(circularView)
         constraintSet.connect(
@@ -173,35 +148,8 @@ internal class CircularBubble {
             ConstraintSet.PARENT_ID,
             ConstraintSet.RIGHT
         )
-        when (chatStyle.embedded_window) {
-            CircularBubbleType.MALE.type->{
-                constraintSet.setMargin(R.id.status_layout, ConstraintSet.RIGHT, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin4)))
-                constraintSet.setMargin(R.id.status_layout, ConstraintSet.TOP, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin4)))
-            }
-            CircularBubbleType.FEMALE.type->{
-                constraintSet.setMargin(R.id.status_layout, ConstraintSet.RIGHT, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin4)))
-                constraintSet.setMargin(R.id.status_layout, ConstraintSet.TOP, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin4)))
-            }
-            CircularBubbleType.CUSTOM_URL.type ->{
-                constraintSet.setMargin(R.id.status_layout, ConstraintSet.RIGHT, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin1)))
-                constraintSet.setMargin(R.id.status_layout, ConstraintSet.TOP, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin1)))
-            }
-            else ->{
-            constraintSet.setMargin(R.id.status_layout, ConstraintSet.RIGHT, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin3)))
-            constraintSet.setMargin(R.id.status_layout, ConstraintSet.TOP, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin3)))
-            }
-        }
-
-//        constraintSet.setMargin(R.id.status_layout, ConstraintSet.RIGHT, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin10)))
-//        constraintSet.setMargin(R.id.status_layout, ConstraintSet.TOP, ScreenUtil().getPixelValue(circularView.context,circularView.context.resources.getDimension(R.dimen.margin10)))
-        if(chatStyle.embedded_window == CircularBubbleType.CUSTOM_URL.type){
-//            constraintSet.setMargin(R.id.status_layout, ConstraintSet.TOP, 10)
-//            constraintSet.setMargin(R.id.status_layout, ConstraintSet.RIGHT, 10)
-        } else {
-//            constraintSet.setMargin(R.id.status_layout, ConstraintSet.TOP, 20)
-//            constraintSet.setMargin(R.id.status_layout, ConstraintSet.RIGHT, 20)
-        }
-
+        constraintSet.setMargin(R.id.status_layout, ConstraintSet.TOP, 20)
+        constraintSet.setMargin(R.id.status_layout, ConstraintSet.RIGHT, 20)
         constraintSet.applyTo(circularView)
     }
 
--- chat_sdk/src/main/java/com/chat/sdk/util/ScreenUtil.kt
+++ chat_sdk/src/main/java/com/chat/sdk/util/ScreenUtil.kt
@@ -5,48 +5,42 @@ import android.content.res.Resources
 import android.util.TypedValue
 
 internal class ScreenUtil {
-    //    fun getScreenWidth(context: Context): Int {
-//        val scale: Float = context.resources.displayMetrics.density
-//        return (60 * scale + 0.5f).toInt()
-//    }
-//
-//    fun getBarBubbleWidth(context: Context): Int {
-//        val displayMetrics = context.resources.displayMetrics
-//       return (2 * displayMetrics.widthPixels)/3
-//    }
-//
-//    fun getBarBubbleHeight(context: Context): Int {
-//        val displayMetrics = context.resources.displayMetrics
-//        return displayMetrics.heightPixels/11
-//    }
-//
-//    fun getCircularBubbleWidth(context: Context): Int {
-//        val displayMetrics = context.resources.displayMetrics
-//        return displayMetrics.widthPixels/6
-//    }
-//
-//    fun getCircularIconWidth(context: Context): Int {
-//        val displayMetrics = context.resources.displayMetrics
-//        return displayMetrics.widthPixels/7
-//    }
-//
-//    fun getImageViewBubbleWidth(context: Context): Int {
-//        val displayMetrics = context.resources.displayMetrics
-//        return displayMetrics.widthPixels/5
-//    }
-//
-//    fun getCustomImageWidth(context: Context): Int {
-//        val displayMetrics = context.resources.displayMetrics
-//        return displayMetrics.widthPixels/7
-//    }
-//
-//    fun isDarkModeEnable(context: Context): Boolean {
-//        val nightModeFlags: Int =context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-//       if(nightModeFlags == Configuration.UI_MODE_NIGHT_YES){
-//           return true
-//       }
-//        return false
-//    }
+        fun getScreenWidth(context: Context): Int {
+        val scale: Float = context.resources.displayMetrics.density
+        return (60 * scale + 0.5f).toInt()
+    }
+
+    fun getBarBubbleWidth(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+       return (2 * displayMetrics.widthPixels)/3
+    }
+
+    fun getBarBubbleHeight(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return displayMetrics.heightPixels/11
+    }
+
+    fun getCircularBubbleWidth(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return displayMetrics.widthPixels/6
+    }
+
+    fun getCircularIconWidth(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return displayMetrics.widthPixels/7
+    }
+
+    fun getImageViewBubbleWidth(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return displayMetrics.widthPixels/5
+    }
+
+    fun getCustomImageWidth(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return displayMetrics.widthPixels/7
+    }
+
+
     fun getMarginInDP(context: Context, size: Float): Int {
         return TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP, size, context.resources.displayMetrics
